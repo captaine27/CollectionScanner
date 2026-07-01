2dd531b63e8dd9d263e54f2157bcba4f7f3cee77
captaine27 <captaine27000@gmail.com>
2026-07-01 13:44:00 +0200

    app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@
 let lecteur = null;
 let dernierCode = "";
 let scannerEnCours = false;
+let codeReader = null;
+let cameraActive = false;
 
 /*****************************************************
  * Initialisation
@@ -41,5 +43,82 @@ function afficherSucces(code){
         <h2>✅ Code détecté</h2>
         <p>${code}</p>
     `;
+
+}
+/*****************************************************
+ * Démarrage du scanner ZXing
+ *****************************************************/
+async function demarrerScanner() {
+
+    if (cameraActive) return;
+
+    cameraActive = true;
+
+    afficherInfo(
+        "📷 Caméra",
+        "Recherche d'un code-barres..."
+    );
+
+    codeReader = new ZXing.BrowserMultiFormatReader();
+
+    try {
+
+        const videoInputDevices =
+            await codeReader.listVideoInputDevices();
+
+        if(videoInputDevices.length === 0){
+
+            afficherInfo(
+                "Erreur",
+                "Aucune caméra trouvée."
+            );
+
+            cameraActive = false;
+            return;
+
+        }
+
+        await codeReader.decodeFromVideoDevice(
+
+            null,
+
+            "camera",
+
+            (result, err) => {
+
+                if(result){
+
+                    const code = result.getText();
+
+                    if(code !== dernierCode){
+
+                        dernierCode = code;
+
+                        navigator.vibrate?.(150);
+
+                        afficherSucces(code);
+
+                    }
+
+                }
+
+            }
+
+        );
+
+    }
+
+    catch(e){
+
+        console.error(e);
+
+        afficherInfo(
+            "Erreur",
+            e.message
+        );
+
+        cameraActive = false;
+
+    }
 
 }
