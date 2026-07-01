7d882ddf0670ce714522056da7caac484a8221d1
captaine27 <captaine27000@gmail.com>
2026-07-01 18:28:44 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,15 @@
+"use strict";
+
 const CONFIG = {
 
+    // Version de l'application
+    VERSION: "2.0",
+
+    // URL Apps Script
     APPS_SCRIPT_URL:
-    "https://script.google.com/macros/s/AKfycbylYfPApNADfZWC5dtFxERcAclqFtaV4EvVK76cfKpO7Ww3k2IqVi1XRHLyim_aZVo/exec"
+    "https://script.google.com/macros/s/AKfycbylYfPApNADfZWC5dtFxERcAclqFtaV4EvVK76cfKpO7Ww3k2IqVi1XRHLyim_aZVo/exec",
+
+    // Durée anti-doublon (ms)
+    DELAI_SCAN: 2000
 
 };
